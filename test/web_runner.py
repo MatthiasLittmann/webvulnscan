5db4ea56975b91542f982060527c26d7ca0364d5
Matthias Littmann <matthias@debian>
2014-05-09 13:23:48 +0200

    Add header: 'Content-type:text/html'

--- test/web_runner.py
+++ test/web_runner.py
@@ -25,6 +25,7 @@ sitemap = {}
 class Handler(BaseHTTPRequestHandler):
     def _default_page(self):
         self.send_response(200)
+        self.send_header('Content-type', 'text/html')
         self.end_headers()
         self.wfile.write("""
         <!DOCTYPE html>
